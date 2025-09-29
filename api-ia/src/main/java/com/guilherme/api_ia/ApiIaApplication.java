5c03d977e212ba66bb33484ceed54e5632e99a20
Guilherme-Crivelaro <crivelarogui@gmail.com>
2025-09-29 15:11:29 -0300

    IA

---
+++ api-ia/src/main/java/com/guilherme/api_ia/ApiIaApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.api_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIaApplication.class, args);
+	}
+
+}
