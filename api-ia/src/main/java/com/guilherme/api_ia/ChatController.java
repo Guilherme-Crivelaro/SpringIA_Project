968fd4394f21ff837f174cb7cc488a8c6473a12d
Guilherme-Crivelaro <crivelarogui@gmail.com>
2025-09-29 15:41:21 -0300

    feat(app): Basic OpenAI chat implementation

--- api-ia/src/main/java/com/guilherme/api_ia/ChatController.java
+++ api-ia/src/main/java/com/guilherme/api_ia/ChatController.java
@@ -1,16 +1,29 @@
 package com.guilherme.api_ia;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
+
+import static java.awt.SystemColor.text;
 
 @RestController
 public class ChatController {
 
     private final ChatClient chatClient;
 
-    public MyController(@NotNull ChatClient.Builder chatClientBuilder){
+    public ChatController(@NotNull ChatClient.Builder chatClientBuilder){
         this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
     }
 }
